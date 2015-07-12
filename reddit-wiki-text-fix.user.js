a17c393c1b26bc0425a81d17651a09895a8d6a1d
Driphter <TheDriphter@gmail.com>
2015-07-12 02:22:10 -0400

    Update reddit-wiki-text-fix.user.js
    IIFE wrap and constant rename.

--- reddit-wiki-text-fix.user.js
+++ reddit-wiki-text-fix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit Wiki Text Fix
 // @namespace    https://github.com/Driphter/UserScripts
-// @version      0.1
+// @version      0.1.1
 // @description  Fixes text in tables that becomes unreadable on Reddit wikis when night mode is enabled.
 // @author       Driphter
 // @match        https://*.reddit.com/r/*/wiki/*
@@ -10,7 +10,9 @@
 // @downloadURL  https://github.com/Driphter/UserScripts/raw/master/reddit-wiki-text-fix.user.js
 // ==/UserScript==
 
-var defaultColor = "rgb(221,221,221)";
+(function(){
+    
+var DEFAULT_COLOR = "rgb(221,221,221)";
 
 function toArray(arr) {
     return [].slice.call(arr);
@@ -20,14 +22,16 @@ function getTextColor() {
     var wikiDiv = toArray(document.getElementsByTagName("div"))
                       .filter(e => e.className == "md wiki")[0];
     
-    if (!wikiDiv) return defaultColor;
+    if (!wikiDiv) return DEFAULT_COLOR;
     
     var p = document.createElement("p");
     wikiDiv.appendChild(p);
     var color = window.getComputedStyle(p).color;
     wikiDiv.removeChild(p);
     
-    return color || defaultColor;
+    return color || DEFAULT_COLOR;
 }
 
 GM_addStyle(".md td, .md th { color: " + getTextColor() + "; }");
+    
+})();
